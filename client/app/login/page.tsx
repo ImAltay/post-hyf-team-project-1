fb0d80d3e715a7003cd1c1955ac010be94d31f43
IamAltay <etliekmek97@gmail.com>
2025-02-01 13:36:49 +0100

    initial forms

---
+++ client/app/login/page.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+type Props = {};
+
+const login = (props: Props) => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900'>
+      <h1 className='text-3xl font-bold mb-6 text-gray-900 dark:text-gray-100'>
+        Login
+      </h1>
+      <form className='bg-white dark:bg-gray-800 p-6 rounded shadow-md w-full max-w-sm'>
+        <div className='mb-4'>
+          <label className='block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2'>
+            Email:
+          </label>
+          <input
+            type='text'
+            name='email'
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 leading-tight focus:outline-none focus:shadow-outline'
+          />
+        </div>
+        <div className='mb-6'>
+          <label className='block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2'>
+            Password:
+          </label>
+          <input
+            type='password'
+            name='password'
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+          />
+        </div>
+        <button
+          type='submit'
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default login;
