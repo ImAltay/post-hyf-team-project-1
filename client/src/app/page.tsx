c1e565dce0b2535f7aa5597bdaa6a6fb08817079
Mariam Isaeva <mariam.hammad8@gmail.com>
2025-02-01 22:14:09 +0100

    files structure

---
+++ client/src/app/page.tsx
@@ -0,0 +1,13 @@
+// import Image from 'next/image';
+// import calImage from '../assets/pic1.webp';
+
+export default function Home() {
+  return (
+    <div>
+      <h3>Header_Component</h3>
+      <h1>Keep track of your calories</h1>
+      {/* <Image src={calImage} alt="home" width={500} height={500} /> */}
+      <h3>Footer_Component</h3>
+    </div>
+  );
+}
