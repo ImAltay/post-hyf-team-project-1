d11ff3bf1fd7acfbd84111d3865d46623867463d
IamAltay <etliekmek97@gmail.com>
2025-02-01 14:44:25 +0100

    forms

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,34 +1,16 @@
+// app/login/page.tsx
 import React from 'react';
+import InputField from '../../components/InputField';
 
-type Props = {};
-
-const login = (props: Props) => {
+const Login = () => {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900'>
       <h1 className='text-3xl font-bold mb-6 text-gray-900 dark:text-gray-100'>
         Login
       </h1>
       <form className='bg-white dark:bg-gray-800 p-6 rounded shadow-md w-full max-w-sm'>
-        <div className='mb-4'>
-          <label className='block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2'>
-            Email:
-          </label>
-          <input
-            type='text'
-            name='email'
-            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 leading-tight focus:outline-none focus:shadow-outline'
-          />
-        </div>
-        <div className='mb-6'>
-          <label className='block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2'>
-            Password:
-          </label>
-          <input
-            type='password'
-            name='password'
-            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 mb-3 leading-tight focus:outline-none focus:shadow-outline'
-          />
-        </div>
+        <InputField label='Email' name='email' type='text' />
+        <InputField label='Password' name='password' type='password' />
         <button
           type='submit'
           className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>
@@ -39,4 +21,4 @@ const login = (props: Props) => {
   );
 };
 
-export default login;
+export default Login;
--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -1,44 +1,69 @@
-import React from 'react';
+'use client';
+import { useState } from 'react';
+import InputField from '../../components/InputField';
 
-type Props = {};
+const Register = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
 
-const register = (props: Props) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://127.0.0.1:5000/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        // Handle successful registration
+        console.log('Registration successful');
+      } else {
+        // Handle errors
+        console.error('Registration failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900'>
       <h1 className='text-3xl font-bold mb-6 text-gray-900 dark:text-gray-100'>
         Register
       </h1>
-      <form className='bg-white dark:bg-gray-800 p-6 rounded shadow-md w-full max-w-sm'>
-        <div className='mb-4'>
-          <label className='block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2'>
-            Name:
-          </label>
-          <input
-            type='text'
-            name='name'
-            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 leading-tight focus:outline-none focus:shadow-outline'
-          />
-        </div>
-        <div className='mb-4'>
-          <label className='block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2'>
-            Email:
-          </label>
-          <input
-            type='text'
-            name='email'
-            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 leading-tight focus:outline-none focus:shadow-outline'
-          />
-        </div>
-        <div className='mb-6'>
-          <label className='block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2'>
-            Password:
-          </label>
-          <input
-            type='password'
-            name='password'
-            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 mb-3 leading-tight focus:outline-none focus:shadow-outline'
-          />
-        </div>
+      <form
+        className='bg-white dark:bg-gray-800 p-6 rounded shadow-md w-full max-w-sm'
+        onSubmit={handleSubmit}>
+        <InputField
+          label='Name'
+          name='username'
+          type='text'
+          onChange={handleChange}
+        />
+        <InputField
+          label='Email'
+          name='email'
+          type='text'
+          onChange={handleChange}
+        />
+        <InputField
+          label='Password'
+          name='password'
+          type='password'
+          onChange={handleChange}
+        />
         <button
           type='submit'
           className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>
@@ -49,4 +74,4 @@ const register = (props: Props) => {
   );
 };
 
-export default register;
+export default Register;
